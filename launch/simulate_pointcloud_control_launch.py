3f0464a0eb41ac07f38e99acc3ae9db49e09e246
nhma <nhma@mmmi.sdu.dk>
2022-02-26 15:41:36 +0100

    added cable yaw files

---
+++ launch/simulate_pointcloud_control_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='iii_drone',
+            executable='vel_ctrl_vec_pub',
+            name='vel_ctrl_vec_pub'
+        ),
+        Node(
+            package='iii_drone',
+            executable='img_3d_to_2d_proj',
+            name='img_3d_to_2d_proj'
+        ),
+        Node(
+            package='iii_drone',
+            #namespace='turtlesim2',
+            executable='lidar_to_mmwave',
+            name='lidar_to_mmwave'
+        )
+    ])
