0b777979455aab521fcab2a1a57691fe00634adf
nhma <nhma@mmmi.sdu.dk>
2022-06-08 10:43:57 +0200

    added PL print+choose script

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -25,7 +25,7 @@ rotation_matrix_t eulToR(orientation_t eul) {
     mat(0,2) = cos_roll*sin_pitch*cos_yaw+sin_roll*sin_yaw;
     mat(1,0) = cos_pitch*sin_yaw;
     mat(1,1) = sin_roll*sin_pitch*sin_yaw+cos_roll*cos_yaw;
-    mat(1,2) = cos_roll*sin_pitch*sin_yaw-sin_roll*cos_pitch;
+    mat(1,2) = cos_roll*sin_pitch*sin_yaw-sin_roll*cos_pitch; // wrong? cos_roll*sin_pitch*sin_yaw-sin_roll*cos_yaw
     mat(2,0) = -sin_pitch;
     mat(2,1) = sin_roll*cos_pitch;
     mat(2,2) = cos_roll*cos_pitch;
