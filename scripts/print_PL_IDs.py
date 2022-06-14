366ddacd1c0dd92045bebb9d1e4e2350fbb8041b
nhma <nhma@mmmi.sdu.dk>
2022-06-14 08:41:16 +0200

    added KF yaw offboard, reduced pl id print callback rate

--- scripts/print_PL_IDs.py
+++ scripts/print_PL_IDs.py
@@ -97,7 +97,7 @@ class IdDrawer(Node):
         )
 
 
-        timer_period = 0.1  # seconds
+        timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.print_IDs)
 
 
