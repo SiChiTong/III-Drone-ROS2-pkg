701986482237239026349c7ac1e3934dd0c03e60
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2022-09-13 15:56:26 +0200

    Import OS

--- launch/pl_mapper_HW.launch.py
+++ launch/pl_mapper_HW.launch.py
@@ -6,6 +6,7 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     config = os.path.join(
